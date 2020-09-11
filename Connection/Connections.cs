ffddc196c67e2a00743dd909c9fa9ea2e693758a
ULYSSESS <ULYSSESS@DESKTOP-MK9933E>
2020-09-11 13:29:23 -0400

    SQL conexion a la base de datos

---
+++ Connection/Connections.cs
@@ -0,0 +1,17 @@
+﻿using LinqToDB.Data;
+using LinqToDB.DataProvider.SqlServer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Connection
+{
+    public class Connections : DataConnection
+    {
+        public Connections() : base(new SqlServerDataProvider("", SqlServerVersion.v2012),
+             "Data Source=DESKTOP-MK9933E\\MK9933E;Database=sales_system;Integrated Security=true;")
+        { }
+    }
+}
